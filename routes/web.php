ba02ca983df535968066da4d5f3070a014069c01
f-cloch333 <yuuchi3bluestar@yahoo.co.jp>
2024-11-27 04:01:10 +0900

    ルート復元

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
- // ルーティングを設定するコントローラを宣言する
- use App\Http\Controllers\HelloController;
- use App\Http\Controllers\ProductController;
- use App\Http\Controllers\VendorController;
- use App\Http\Controllers\RequestController;
- use App\Http\Controllers\ResponseController;
- use App\Http\Controllers\SignInController;
- use App\Http\Controllers\CookieController;
- use App\Http\Controllers\SessionController;
-
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\SignInController;
+use App\Http\Controllers\CookieController;
+use App\Http\Controllers\SessionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -69,4 +67,3 @@ Route::get('/sessions/create', [SessionController::class, 'create'])->name('sess
 Route::post('/sessions/store', [SessionController::class, 'store'])->name('sessions.store');
 
 Route::delete('/sessions/destroy', [SessionController::class, 'destroy'])->name('sessions.destroy');
-
