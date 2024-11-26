3fa2ca3577d22c1697295b4d77394441f002ab68
f-cloch333 <yuuchi3bluestar@yahoo.co.jp>
2024-11-27 05:52:57 +0900

    web.phpの設定

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,3 +19,18 @@ Route::get('/', function () {
 });
 
 require __DIR__.'/auth.php';
+// 会員登録
+Route::get('register', [RegisteredUserController::class, 'create'])
+    ->name('register');
+Route::post('register', [RegisteredUserController::class, 'store']);
+
+// ログイン
+Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    ->name('login');
+Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+// パスワード再設定
+Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+    ->name('password.request');
+Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+    ->name('password.email');
