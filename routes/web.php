2bebbfda11b7ae7adcc4a3a1eed908a7e9c2859a
f-cloch333 <yuuchi3bluestar@yahoo.co.jp>
2024-11-30 09:43:36 +0900

    WEB.phpをもどす

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 require __DIR__.'/auth.php';
+
+
