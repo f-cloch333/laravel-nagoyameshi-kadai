7db924fed64c180854cf00f00ff4b0b47cc83ed3
f-cloch333 <yuuchi3bluestar@yahoo.co.jp>
2024-12-05 07:55:43 +0900

    修正

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,18 +17,17 @@ class UserSeeder extends Seeder
     {
         User::factory()->count(100)->create();
 
-                // 一般会員のアカウントを作成
-                User::create([
-                    'name' => '一般ユーザー',
-                    'email' => 'user@example.com',
-                    'password' => Hash::make('password123'), // パスワードはハッシュ化
-                    'postal_code' => '1234567',
-                    'address' => '東京都',
-                    'phone_number' => '08012345678',
-                    'birthday' => '1990-01-01',
-                    'occupation' => '会社員',
-                ]);
-            
+        User::create([
+        'name' => '山田',
+        'kana' => 'ヤマダ',
+        'email' => 'user@example.com',
+        'password' => Hash::make('password123'), // パスワードはハッシュ化
+        'postal_code' => '1234567',
+        'address' => '東京都',
+        'phone_number' => '08012345678',
+        'birthday' => '1990-01-01',
+        'occupation' => '会社員',
+    ]);
 
     }
 }
