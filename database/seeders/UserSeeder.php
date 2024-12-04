4c42350a99e6e06efc46d0bdda2731161a01059f
f-cloch333 <153550519+f-cloch333@users.noreply.github.com>
2024-12-04 19:14:08 +0900

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,5 +16,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(100)->create();
+
+         $user = new User();
+         $user->name = "田中太郎";
+         $user->kana = "タナカタロウ";
+         $user->email = 'tarou1204@example.com';
+         $user->email_verified_at = Carbon::now();
+         $user->password = Hash::make('password');
+         $user->postal_code = "101-0022";
+         $user->address = "東京都";
+         $user->phone_number = "090-5554-1118";
+         $user->save();
     }
 }
