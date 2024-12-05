3f7530e8695ddbbf83f6ae0e5360c303e5392183
f-cloch333 <yuuchi3bluestar@yahoo.co.jp>
2024-12-05 11:41:12 +0900

    アドミン修正

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -20,9 +20,9 @@ public function run(): void
         $admin->save();
 
     // 課題閲覧用管理者アカウント
-        $asmin = new Admin();
-        $admin->email = 'admin2@example.com';
-        $admin->password = Hash::make('nagoyameshi2');
-        $admin->save();
+        $asmin2 = new Admin();
+        $admin2->email = 'admin2@example.com';
+        $admin2->password = Hash::make('nagoyameshi2');
+        $admin2->save();
     }
 }
